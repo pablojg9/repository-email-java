7c836771b52591c12122b4713ce90b7b95a177e6
pablojg9 <pablojuniorgn2@gmail.com>
2021-08-04 14:27:26 -0300

    Projeto de envio de email com java

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -9,6 +9,12 @@ public class AppTest {
 
         Properties properties = new Properties();
 
+        properties.put("mail.smtp.auth", "true"); //Autorização
+        properties.put("mail.smtp.starttls", "true"); // Autenticação
+        properties.put("mail.smtp.host", "stmp.gmail.com"); //Servidor gmail Google
+        properties.put("mail.smtp.port", "465"); // Porta do servidor
+        properties.put("mail.smtp.socketFactory.port", "465");
+
 
 
     }
