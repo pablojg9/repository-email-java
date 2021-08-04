c023853b93857ab8b95eabd2e71a15349e18e2ab
pablojg9 <pablojuniorgn2@gmail.com>
2021-08-04 14:18:50 -0300

    Projeto de envio de email com java

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+import java.util.Properties;
+
+public class AppTest {
+
+    @Test
+    public void testEmail() {
+
+        Properties properties = new Properties();
+
+
+
+    }
+}
