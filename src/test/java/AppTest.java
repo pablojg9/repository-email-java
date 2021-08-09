3340004dbb7dc0ee1a731ba35cdeb54228b5518f
pablojg9 <pablojuniorgn2@gmail.com>
2021-08-09 09:48:45 -0300

    Projeto de envio de email com java

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -8,7 +8,12 @@ public class AppTest {
 
       StringBuilder stringBuilder = new StringBuilder();
 
-      stringBuilder.append("testando html");
+      stringBuilder.append("<h1>Seja Bem-Vindo</h1><br/></br>");
+      stringBuilder.append("<a target=\"_blank\" href=\"https://github.com/pablojg9\">MEU GITHUB</a>");
+
+      stringBuilder.append("<p style=\"color: #ccc\">Um texto aparece aqui. Texto falando sobre <strong>JAVA</strong> onde a linguagem de " +
+              "programação podemos fazer de tudo.</p>");
+
 
         try {
             EmailSend emailSend = new EmailSend(
